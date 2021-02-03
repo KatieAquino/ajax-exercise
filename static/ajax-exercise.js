ea0ba810c0e06378fa5730e5f3b36cb41045311a
Katie Aquino <katiemaryfrances.aquino@gmail.com>
2021-02-03 04:13:50 +0000

    Refactor JS functions

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -1,33 +1,25 @@
 "use strict";
 
-
-function replaceFortune(result) {
-    $("#fortune-text").html(result);
-}
-
+//Show random fortune
 function showFortune(evt) {
 
-    $.get('/fortune', replaceFortune);
+    $.get('/fortune', (result) => {
+        $("#fortune-text").html(result);
+    });
 }
-
 $('#get-fortune-button').on('click', showFortune);
 
 
-
-
-
-// PART 2: SHOW WEATHER
-function replaceWeathers(results) {
-   $("#weather-info").html(results.forecast); 
-}
+// SHOW WEATHER Forcast
 function showWeather(evt) {
     evt.preventDefault();
 
     let url = "/weather.json";
     let formData = {"zipcode": $("#zipcode-field").val()};
 
-    $.get(url,formData, replaceWeathers);
-    // TODO: request weather with that URL and show the forecast in #weather-info
+    $.get(url, formData, (res) => {
+        $('#weather-info').text(res.forecast);
+    });
 }
 
 $("#weather-form").on('submit', showWeather);
@@ -35,30 +27,24 @@ $("#weather-form").on('submit', showWeather);
 
 
 
-// PART 3: ORDER MELONS
+// Show result message after submitting order form
+// If result is error, show in red
 
 function orderMelons(evt) {
     evt.preventDefault();
 
     let url = "/order-melons.json";
-    // let formInputs = {
-    //     'qty': $('#qty-field').val(),
-    //     'melon': $('#melon-type-field').val(),
-    // };
-
     let formValues = $('#order-form').serialize();
 
-    // TODO: show the result message after your form
     $.post(url, formValues, (res) => {
         if (res.code === 'ERROR') {
             $('#order-status').addClass("order-error");
-            // $('#order-status').text(res.code);
+            $('#order-status').prepend(res.code);
+            console.log(res.code)
         };
-
         $('#order-status').text(res.msg);
         console.log(res);
     });
-    // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
 }
 
 $("#order-form").on('submit', orderMelons);
